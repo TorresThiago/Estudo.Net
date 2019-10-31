adfc7548d9bcb5153316e749237f9bbdcbbe29bc
Thiago Torres <torres.thiago.rj@gmail.com>
2019-10-31 00:52:30 -0300

    .net

---
+++ Aula07/Projeto.DAL/Repositories/ClienteRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto.DAL.Entities;
+using System.Data.SqlClient;
+
+namespace Projeto.DAL.Repositories
+{
+    public class ClienteRepository : Conexao
+    {
+        public void Insert(Cliente c)
+        {
+            OpenConnection();
+
+            string query = "INSERT INTO CLIENTE(NOME, EMAIL, DATACADASTRO)"
+                          +"VALUES(@NOME, @EMAIL, GetDate())";
+
+            cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@Nome", c.Nome);
+            cmd.Parameters.AddWithValue("@Email", c.Email);
+            cmd.ExecuteNonQuery();
+
+            CloseConnection();
+        }
+
+        public bool HasEmail(string email)
+        {
+            OpenConnection();
+
+            string query = "SELECT COUNT(*) FROM CLIENTE "
+                           +"WHERE EMAIL = @EMAIL";
+
+            cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@EMAIL", email);
+            int count = (int)(cmd.ExecuteScalar());
+
+            CloseConnection();
+
+            return count > 0;
+        }
+    }
+}
+
---
+++ Aula07/Projeto.DAL/Repositories/Conexao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Projeto.DAL.Repositories
+{
+    public class Conexao
+    {
+        protected SqlConnection con; //Classe para conexão BD
+        protected SqlDataReader dr;  //Classe para fazer a leitura dos dados (Select)
+        protected SqlCommand cmd;    //Classe para executar comandos SQL no BD (Insert, Update, etc)
+        protected SqlTransaction tr; //Classe utilizada para trabalhar com as transações no BD (Commit ou rollback)
+
+        protected void OpenConnection()
+        {
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnBD"].ConnectionString);
+            con.Open();
+        }
+
+        protected void CloseConnection()
+        {            
+            con.Close();
+        }
+    }
+}
