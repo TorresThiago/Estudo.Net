7e47a6b4259367a92a5df21ccaafe6bf177c1938
thiago.torres.ter <thiago.torres.ter@estacio.br>
2019-11-08 11:11:48 -0300

    Estudo

---
+++ Aula10/Projeto.DAL/Repositorios/ProdutoRepositorio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Projeto.DAL.Entidades;
+
+namespace Projeto.DAL.Repositorios
+{
+    public class ProdutoRepositorio : Conexao
+    {
+        public void Insert(Produto p)
+        {
+            OpenConection();
+
+            string query = "insert into Produto " +
+                            "values (@Nome, @Preco, @Quantidade) ";
+
+            cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@Nome",p.Nome);
+            cmd.Parameters.AddWithValue("@Preco", p.Preco);
+            cmd.Parameters.AddWithValue("@Quantidade", p.Quantidade);
+            cmd.ExecuteNonQuery();
+
+            CloseConnection();
+        }
+
+    }
+}
---
+++ Aula10/Projeto.WEB/Controllers/ProdutoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projeto.DAL.Entidades;
+using Projeto.DAL.Repositorios;
+using Projeto.WEB.Models;
+
+namespace Projeto.WEB.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public JsonResult CadastrarProduto(ProdutoCadastroModel produtoModel)
+        {
+            try
+            {
+                Produto p = new Produto();
+                p.Nome = produtoModel.Nome;
+                p.Preco = produtoModel.Preco;
+                p.Quantidade = produtoModel.Quantidade;
+
+                ProdutoRepositorio rep = new ProdutoRepositorio();
+                rep.Insert(p);
+
+                return Json($"Produto {p.Nome} cadastrado.");
+
+            }
+            catch (Exception e)
+            {
+                return Json(e.Message);
+            }
+        }
+    }
+}
